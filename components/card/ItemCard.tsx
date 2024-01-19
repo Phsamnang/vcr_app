6fd9bcceefca94d00aae17d8e6402fbbd2323139
unknown <samnangldn@gmail.com>
2024-01-19 16:02:09 +0700

    update card

--- components/card/ItemCard.tsx
+++ components/card/ItemCard.tsx
@@ -3,8 +3,8 @@ import classes from "./ItemCard.module.css";
 export const ItemCard = ({data}) => {
 
     return <>
-        <div className="card" style={{ width: "18rem" }}>
-            <img src={data?.image} className="card-img-top" alt="..." />
+        <div className="card col" style={{maxWidth:180}}>
+            <img src={data?.image} className={`card-img-top w-100 ${classes.img}`} alt="..." />
             <div className="card-body">
                 <h5 className="card-title">{data?.productName}</h5>
                 <p className="card-text">
