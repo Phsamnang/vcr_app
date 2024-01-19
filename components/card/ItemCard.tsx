66d3a799829e4416ac3c18e3f996d314478fd487
unknown <samnangldn@gmail.com>
2024-01-19 16:57:12 +0700

    config next

--- components/card/ItemCard.tsx
+++ components/card/ItemCard.tsx
@@ -1,10 +1,11 @@
 
 import classes from "./ItemCard.module.css";
+import Image from "next/image";
 export const ItemCard = ({data}) => {
 
     return <>
         <div className="card col" style={{maxWidth:180}}>
-            <img src={data?.image} className={`card-img-top w-100 ${classes.img}`} alt="..." />
+            <Image src={data?.image} className="card-img-top" width={180} height={140} />
             <div className="card-body">
                 <h5 className="card-title">{data?.productName}</h5>
                 <p className="card-text">
